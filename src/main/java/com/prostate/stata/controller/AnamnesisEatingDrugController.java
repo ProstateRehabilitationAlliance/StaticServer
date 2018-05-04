67ecae193b071b8956769f5db3e55ce4e9208792
MaxCoderCh <MaxCoderCh@users.noreply.github.com>
2018-05-04 18:54:45 +0800

    常用药物相关接口修改添加
    1. 模糊查询常用药物 接口优化
    2. 单个常用药物添加 接口添加
    3. 批量常用药物添加 接口添加

--- src/main/java/com/prostate/stata/controller/AnamnesisEatingDrugController.java
+++ src/main/java/com/prostate/stata/controller/AnamnesisEatingDrugController.java
@@ -3,6 +3,7 @@ package com.prostate.stata.controller;
 
 import com.prostate.stata.entity.AnamnesisEatingDrug;
 import com.prostate.stata.service.AnamnesisEatingDrugService;
+import com.prostate.stata.util.SpellUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ import java.util.Map;
 @Slf4j
 @RestController
 @RequestMapping(value = "anamnesisEatingDrug")
-public class AnamnesisEatingDrugController extends BaseController{
+public class AnamnesisEatingDrugController extends BaseController {
 
     @Autowired
     private AnamnesisEatingDrugService anamnesisEatingDrugService;
@@ -24,18 +25,15 @@ public class AnamnesisEatingDrugController extends BaseController{
 
     /**
      * 模糊查询正在服用的药物
+     *
      * @return
      */
     @PostMapping(value = "search")
-    public Map search(String spellName){
+    public Map search(String spellName) {
         log.info("#########模糊查询正在服用的药物########");
-        resultMap = new LinkedHashMap<>();
         //参数校验
-        if (spellName==null||"".equals(spellName)){
-            resultMap.put("code","20001");
-            resultMap.put("msg","PARAM_EMPTY");
-            resultMap.put("result",null);
-            return resultMap;
+        if (spellName == null || "".equals(spellName)) {
+            return emptyParamResponse();
         }
         //创建查询条件
         AnamnesisEatingDrug anamnesisEatingDrug = new AnamnesisEatingDrug();
@@ -45,17 +43,66 @@ public class AnamnesisEatingDrugController extends BaseController{
         List<AnamnesisEatingDrug> anamnesisEatingDrugList = anamnesisEatingDrugService.selectByParams(anamnesisEatingDrug);
 
         //查询结果不为空时请求响应
-        if (anamnesisEatingDrugList!=null&&anamnesisEatingDrugList.size()>0){
-            resultMap.put("code","20000");
-            resultMap.put("msg","SUCCESS");
-            resultMap.put("result",anamnesisEatingDrugList);
-            return resultMap;
+        if (anamnesisEatingDrugList != null && anamnesisEatingDrugList.size() > 0) {
+
+            return querySuccessResponse(anamnesisEatingDrugList);
         }
         //查询结果为空时请求响应
-        resultMap.put("code","20002");
-        resultMap.put("msg","RESULT_EMPTY");
-        resultMap.put("result",null);
-        return resultMap;
+        return queryEmptyResponse();
 
+    }
+
+
+    /**
+     * 单个添加常用药物
+     * @param drugName
+     * @return
+     */
+    @PostMapping(value = "insert")
+    public Map insert(String drugName) {
+        //参数校验
+        if (drugName == null || "".equals(drugName)) {
+            //参数不合法返回信息
+            return emptyParamResponse();
+
+        }
+        //创建常用药品对象
+        AnamnesisEatingDrug anamnesisEatingDrug = new AnamnesisEatingDrug();
+        //常用药品对象赋值
+        anamnesisEatingDrug.setEatingDrugName(drugName);
+        anamnesisEatingDrug.setSpellName(SpellUtils.getPinYinHeaderChar(drugName));
+        anamnesisEatingDrug.setEatingDrugNumber("0");
+        //常用药品对象添加
+       int i = anamnesisEatingDrugService.insertSelective(anamnesisEatingDrug);
+       //药品添加结果校验
+       if(i>=0){
+           return insertSuccseeResponse();
+       }
+        return insertFailedResponse();
+    }
+
+    /**
+     * 批量添加常用药物
+     * @param drugNames
+     * @return
+     */
+    @PostMapping(value = "insertStrs")
+    public Map insertStrs(String drugNames) {
+
+        resultMap = new LinkedHashMap<>();
+        if (drugNames == null || "".equals(drugNames)) {
+           return emptyParamResponse();
+        }
+        String[] strs = drugNames.replaceAll("\\s*", "").split("#");
+        for (String s : strs) {
+            //创建插入对象
+            AnamnesisEatingDrug anamnesisEatingDrug = new AnamnesisEatingDrug();
+            anamnesisEatingDrug.setEatingDrugName(s);
+            anamnesisEatingDrug.setSpellName(SpellUtils.getPinYinHeaderChar(s).replace("(", "").replace(")", ""));
+            anamnesisEatingDrug.setEatingDrugNumber("0");
+
+            anamnesisEatingDrugService.insertSelective(anamnesisEatingDrug);
+        }
+        return insertSuccseeResponse();
     }
 }
